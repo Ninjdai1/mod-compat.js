3a9478345af5dc6a9335a79ff99c5c71623567fe
Ninjdai <ninjdai@duck.com>
2024-03-28 10:46:36 +0100

    Ability to copy data from custom folder

--- index.mjs
+++ index.mjs
@@ -1,5 +1,10 @@
 import { loadData } from "./helpers.mjs";
 import { generateRecipes } from "./recipes.mjs";
+import fs from "fs";
 const modsData = loadData();
 
 generateRecipes(modsData);
+fs.cp('./custom/', './output/', {recursive: true}, (err) => {
+    if(err)
+        console.error(err)
+});
