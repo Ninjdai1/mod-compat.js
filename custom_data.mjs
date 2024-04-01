9f8d124678a4746498a3b0e3c8e129774f6f5b15
Ninjdai <ninjdai@duck.com>
2024-04-01 13:18:19 +0200

    Add bakery cakes and pies cutting compat

--- custom_data.mjs
+++ custom_data.mjs
@@ -1,40 +1,74 @@
 const cuttables = {
+    bakery: [
+        {
+            input: [{item: "bakery:apple_pie"}],
+            output: [{item: "bakery:apple_pie_slice", count: 4}],
+        },
+        {
+            input: [{item: "bakery:strawberry_cake"}],
+            output: [{item: "bakery:strawberry_cake_slice", count: 4}],
+        },
+        {
+            input: [{item: "bakery:sweetberry_cake"}],
+            output: [{item: "bakery:sweetberry_cake_slice", count: 4}],
+        },
+        {
+            input: [{item: "bakery:chocolate_cake"}],
+            output: [{item: "bakery:chocolate_cake_slice", count: 4}],
+        },
+        {
+            input: [{item: "bakery:bundt_cake"}],
+            output: [{item: "bakery:bundt_cake_slice", count: 4}],
+        },
+        {
+            input: [{item: "bakery:linzer_tart"}],
+            output: [{item: "bakery:linzer_tart_slice", count: 4}],
+        },
+        {
+            input: [{item: "bakery:glowberry_tart"}],
+            output: [{item: "bakery:glowberry_tart_slice", count: 4}],
+        },
+        {
+            input: [{item: "bakery:chocolate_tart"}],
+            output: [{item: "bakery:chocolate_tart_slice", count: 4}],
+        },
+    ],
     meadow: [
         {
             input: [{item: "meadow:cheesecake"}],
-            output: [{item: "meadow:cheesecake_slice", count: 4}]
+            output: [{item: "meadow:cheesecake_slice", count: 4}],
         },
         {
             input: [{item: "meadow:cheese_tart"}],
-            output: [{item: "meadow:cheese_tart_slice", count: 4}]
+            output: [{item: "meadow:cheese_tart_slice", count: 4}],
         },
         {
             input: [{item: "meadow:cheese_block"}],
-            output: [{item: "meadow:piece_of_cheese", count: 4}]
+            output: [{item: "meadow:piece_of_cheese", count: 4}],
         },
         {
             input: [{item: "meadow:amethyst_cheese_block"}],
-            output: [{item: "meadow:piece_of_amethyst_cheese", count: 4}]
+            output: [{item: "meadow:piece_of_amethyst_cheese", count: 4}],
         },
         {
             input: [{item: "meadow:buffalo_cheese_block"}],
-            output: [{item: "meadow:piece_of_buffalo_cheese", count: 4}]
+            output: [{item: "meadow:piece_of_buffalo_cheese", count: 4}],
         },
         {
             input: [{item: "meadow:goat_cheese_block"}],
-            output: [{item: "meadow:piece_of_goat_cheese", count: 4}]
+            output: [{item: "meadow:piece_of_goat_cheese", count: 4}],
         },
         {
             input: [{item: "meadow:grain_cheese_block"}],
-            output: [{item: "meadow:piece_of_grain_cheese", count: 4}]
+            output: [{item: "meadow:piece_of_grain_cheese", count: 4}],
         },
         {
             input: [{item: "meadow:sheep_cheese_block"}],
-            output: [{item: "meadow:piece_of_sheep_cheese", count: 4}]
+            output: [{item: "meadow:piece_of_sheep_cheese", count: 4}],
         },
         {
             input: [{item: "meadow:grain_cheese_block"}],
-            output: [{item: "meadow:piece_of_warped_cheese", count: 4}]
+            output: [{item: "meadow:piece_of_warped_cheese", count: 4}],
         },
     ]
 }
