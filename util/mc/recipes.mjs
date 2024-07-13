6d83da0a823a2f67ecaae17d64ff38752701eb0c
Ninjdai <ninjdai@duck.com>
2024-07-13 20:52:51 +0200

    fix: Crash on empty conditional recipes

--- util/mc/recipes.mjs
+++ util/mc/recipes.mjs
@@ -5,6 +5,7 @@ function getRecipesFromFiles(files){
         if (files.hasOwnProperty(path) && path.includes("/recipes/") && !path.includes("/advancements/recipes/") && path.endsWith(".json")) {
             let json = JSON.parse(files[path].toString());
             if(json.type == "forge:conditional" || json.type == "neoforge:conditional"){
+                if(!(json.recipes[0])) continue;
                 json = json.recipes[0].recipe;
             } else if(json.type == "doapi:conditional"){
                 json = json.recipe
